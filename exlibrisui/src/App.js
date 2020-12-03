bea03efdcdd6786363abe64da85b14f88ead16bc
Tetiana <ttobin@protonmail.ch>
2020-12-03 21:17:47 +0000

    Favicon added, minor changes

--- exlibrisui/src/App.js
+++ exlibrisui/src/App.js
@@ -1,23 +1,15 @@
+import { Component } from 'react';
+import { render } from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello from React</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
