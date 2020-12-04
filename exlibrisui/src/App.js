f5929277860dcb705f16116554f2c84e4b5d6a81
Tetiana <ttobin@protonmail.ch>
2020-12-04 17:50:30 +0000

    Eslint, prettier installed and configured

--- exlibrisui/src/App.js
+++ exlibrisui/src/App.js
@@ -1,15 +1,10 @@
-import { Component } from 'react';
-import { render } from 'react-dom';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello from React</h1>
-      </div>
-    )
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello from React</h1>
+    </div>
+  );
 }
-
-export default App;
