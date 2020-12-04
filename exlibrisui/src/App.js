2e83a8691b8670e1d505393f338874e7984c16b3
Tetiana <ttobin@protonmail.ch>
2020-12-04 19:05:20 +0000

    Book component created

--- exlibrisui/src/App.js
+++ exlibrisui/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Book from './Book/Book';
 
 export default function App() {
   return (
     <div className="App">
+      <Book />
       <h1>Hello from React</h1>
     </div>
   );
--- exlibrisui/src/Book/Book.js
+++ exlibrisui/src/Book/Book.js
@@ -3,10 +3,9 @@ import React from 'react';
 const Book = () => {
   return (
     <div>
-      
+      <h1>Book1</h1>
     </div>
-  )
-}
+  );
+};
 
 export default Book;
-
