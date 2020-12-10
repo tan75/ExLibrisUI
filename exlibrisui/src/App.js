c3a642bd6601c02a90f41c1603aec972ef762d20
Tetiana <ttobin@protonmail.ch>
2020-12-10 19:52:54 +0000

    Reset

--- exlibrisui/src/App.js
+++ exlibrisui/src/App.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
-import './App.css';
-import Book from './Book/Book';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Book />
-    </div>
-  );
-};
-
-export default App;
+export default class App extends Component {
+  render() {
+    return <div>HEllo</div>;
+  }
+}
