ebb2148ac9e4d97848e01d9e3a7414d3ea0a8d90
Tetiana <ttobin@protonmail.ch>
2020-12-12 21:42:31 +0000

    Revert "Update App.js"
    This reverts commit 78b786dd7a59e0cbde326518763f540d7c6b6b31.

--- exlibrisui/src/App.js
+++ exlibrisui/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
 import Jumbotron from 'react-bootstrap/Jumbotron';
-import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import Books from './components/Books';
 
 class App extends Component {
@@ -26,26 +26,14 @@ class App extends Component {
       <Container className="p-3">
         <Jumbotron>
           <h1 className="header">Welcome to ExLibris</h1>
-          <Card style={{ width: '18rem' }}>
-            <Card.Body>
-              <Card.Title>
-                <Books
-                  books={books.map((book) => (
-                    <div>{book.title}</div>
-                  ))}
-                />
-              </Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                Card Subtitle
-              </Card.Subtitle>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the cards content.
-              </Card.Text>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
-            </Card.Body>
-          </Card>
+          <div>
+            <Books
+              books={books.map((book) => (
+                <div>{book.title}</div>
+              ))}
+            />
+            <Button variant="primary">Learn more</Button>
+          </div>
         </Jumbotron>
       </Container>
     );
