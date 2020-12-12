f24f308e322939c21f916fcdff860b5b9b6ba636
Tetiana <ttobin@protonmail.ch>
2020-12-12 21:42:12 +0000

    Revert "Bootstrap Added"
    This reverts commit 8ac4f841c40ffd5547fc683ce410148e99db4493.

--- exlibrisui/src/App.js
+++ exlibrisui/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Books from './components/Books';
 
 class App extends Component {
@@ -19,21 +17,6 @@ class App extends Component {
         title: 'JavaScript Bad Parts',
         pages: 10,
       },
-      {
-        id: 3,
-        title: 'Python Good Parts',
-        pages: 10002,
-      },
-      {
-        id: 4,
-        title: 'Python Bad Parts',
-        pages: 10,
-      },
-      {
-        id: 5,
-        title: 'Java Good Parts',
-        pages: 10002,
-      },
     ],
   };
 
@@ -43,28 +26,26 @@ class App extends Component {
       <Container className="p-3">
         <Jumbotron>
           <h1 className="header">Welcome to ExLibris</h1>
-          <Row>
-            <Books
-              books={books.map((book) => (
-                <Col>
-                  <Card style={{ width: '18rem' }}>
-                    <Card.Body>
-                      <Card.Title>{book.title}</Card.Title>
-                      <Card.Subtitle className="mb-2 text-muted">
-                        Card Subtitle
-                      </Card.Subtitle>
-                      <Card.Text>
-                        Some quick example text to build on the card title and
-                        make up the bulk of the cards content.
-                      </Card.Text>
-                      <Card.Link href="#">Card Link</Card.Link>
-                      <Card.Link href="#">Another Link</Card.Link>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              ))}
-            />
-          </Row>
+          <Card style={{ width: '18rem' }}>
+            <Card.Body>
+              <Card.Title>
+                <Books
+                  books={books.map((book) => (
+                    <div>{book.title}</div>
+                  ))}
+                />
+              </Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Card Subtitle
+              </Card.Subtitle>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the cards content.
+              </Card.Text>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+          </Card>
         </Jumbotron>
       </Container>
     );
