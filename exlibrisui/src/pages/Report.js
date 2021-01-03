9280c1afff4f4f10a93c2f67d89dc783a1831f48
Tetiana <ttobin@protonmail.ch>
2021-01-03 21:16:11 +0000

    Update Report.js

--- exlibrisui/src/pages/Report.js
+++ exlibrisui/src/pages/Report.js
@@ -1,6 +1,13 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 
 export default class Report extends Component {
+  componentDidMount() {
+    axios
+      .get('http://localhost:8000/api/report')
+      .then((res) => console.log(res));
+  }
+
   render() {
     return (
       <div>
