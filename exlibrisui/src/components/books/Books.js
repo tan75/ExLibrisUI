e882daef46f6c3c156ba20eebe65aedb16bf7c34
Tetiana <ttobin@protonmail.ch>
2021-01-23 21:15:38 +0000

    Revert "Configure Redux for getBooks"
    This reverts commit cc3b1d4115d2b38ece41101aa8001f5777780971.

--- exlibrisui/src/components/books/Books.js
+++ exlibrisui/src/components/books/Books.js
@@ -1,20 +1,11 @@
-import { connect } from 'react-redux';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { getBooks } from '../../store/actions/booksActions';
 import BookItem from './BookItem';
 
 class Books extends Component {
-  componentDidMount() {
-    this.props.getBooks();
-  }
-
   render() {
     const { books, addToReport, deleteBook } = this.props;
-    console.log('556 this.props => ', this.props);
-    console.log('557 books => ', typeof books, ' ', JSON.stringify(books));
-    const updBooks = books.books;
-    return updBooks.map((book) => (
+    return books.map((book) => (
       <BookItem
         key={book._id}
         book={book}
@@ -29,10 +20,6 @@ Books.propTypes = {
   books: PropTypes.array.isRequired,
   addToReport: PropTypes.func.isRequired,
   deleteBook: PropTypes.func.isRequired,
-  getBooks: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = (state) => ({ books: state.books });
-
-// export default Books;
-export default connect(mapStateToProps, { getBooks })(Books);
+export default Books;
--- exlibrisui/src/store/store.js
+++ exlibrisui/src/store/store.js
@@ -1,23 +1,2 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-import rootReducer from './reducers';
-
-const initialState = {
-  books: [],
-  inputValue: '',
-};
-
-const middleware = [thunk];
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  rootReducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
-
-console.log('009 store.js initial state ', initialState);
-
-export default store;
