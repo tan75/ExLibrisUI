4c42c0bf0b1d9a39c2ac220207255e8600690eb7
Tetiana <ttobin@protonmail.ch>
2021-01-24 09:47:44 +0000

    Revert "Configure Redux for getBooks"
    This reverts commit cc3b1d4115d2b38ece41101aa8001f5777780971.

--- exlibrisui/src/index.js
+++ exlibrisui/src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Provider } from 'react-redux';
-import store from './store/store';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
