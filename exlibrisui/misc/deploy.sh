fefc49060de9eaeee0ba1cf8bfbbcdbf6ac007d3
Tetiana <ttobin@protonmail.ch>
2021-03-23 08:24:19 +0000

    Update deploy.sh

--- exlibrisui/misc/deploy.sh
+++ exlibrisui/misc/deploy.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
-HOST=$1
+USER=$1
+HOST=$2
 
 # Build frontend
 echo 'Running >> npm build >> ...'
 
-npm run build
+#npm run build
 
 # Copy to the remote server
-ssh $HOST
+ssh $USER@$HOST cd "../../var/www 
+mkdir test2
+
+scp -r ./build/* root@161.35.124.133:/var/www/html
+
+
+
+
+# Log out from droplet
+
