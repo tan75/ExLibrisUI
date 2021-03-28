5b6db2e51ce54b4f6058ba846f21e48aa7837656
Tetiana <ttobin@protonmail.ch>
2021-03-29 00:39:03 +0100

    Update MainContent.js

--- exlibrisui/src/components/MainContent/MainContent.js
+++ exlibrisui/src/components/MainContent/MainContent.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom;';
+import { Switch, Redirect, Route } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import About from '../../pages/About/About';
 import BookList from '../../pages/BookList/BookList';
 
-import { ROUTE_ABOUT } from '../../constants';
+import { ROUTE_ABOUT, ROUTE_BOOKS, ROUTE_HOME } from '../../constants';
 
 const MainContent = () => {
   return (
     <div className="MainContent mt-3">
       <Container>
         <Switch>
-          <Route path={ROUTE_ABOUT}>
-            <About />
+          <Route path={ROUTE_BOOKS}>
+            <BookList />
+          </Route>
+          <Route path={ROUTE_ABOUT} component={About} />
+          <Route path={ROUTE_HOME}>
+            <Redirect to={ROUTE_BOOKS} />
           </Route>
         </Switch>
-        <BookList />
       </Container>
     </div>
   );
