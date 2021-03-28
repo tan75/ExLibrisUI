3618b34bb4385f1655aa1b0973474b76693e4d26
Tetiana <ttobin@protonmail.ch>
2021-03-29 00:10:59 +0100

    Add About

--- exlibrisui/src/components/MainContent/MainContent.js
+++ exlibrisui/src/components/MainContent/MainContent.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom;';
 import Container from 'react-bootstrap/Container';
+import About from '../../pages/About/About';
 import BookList from '../../pages/BookList/BookList';
+
+import { ROUTE_ABOUT } from '../../constants';
 
 const MainContent = () => {
   return (
     <div className="MainContent mt-3">
       <Container>
+        <Switch>
+          <Route path={ROUTE_ABOUT}>
+            <About />
+          </Route>
+        </Switch>
         <BookList />
       </Container>
     </div>
