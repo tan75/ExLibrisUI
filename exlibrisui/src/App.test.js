79aec7f1393b9bcd5f305cbd15020dc265a39868
Tetiana <ttobin@protonmail.ch>
2021-03-31 11:06:04 +0100

    Upd

--- exlibrisui/src/App.test.js
+++ exlibrisui/src/App.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+import Enzyme, { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import MainNav from './components/MainNav/MainNav';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 test('renders learn react link', () => {
   render(<App />);
   // const linkElement = screen.getByText(/learn react/i);
   // expect(linkElement).toBeInTheDocument();
+});
+
+describe('<MainNav />', () => {
+  it('should render MainNav', () => {
+    const wrapper = shallow(<MainNav />);
+    console.log(wrapper);
+    expect(wrapper.find(<MainNav />)).toHaveLength(1);
+  });
 });
--- exlibrisui/src/components/MainNav/MainNav.test.js
+++ exlibrisui/src/components/MainNav/MainNav.test.js
@@ -4,15 +4,12 @@ import Enzyme, { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import MainNav from './MainNav';
-import Paragraph from './Paragraph';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<MainNav />', () => {
-  it('should render 5 navigation items', () => {
-    const wrapper = shallow(<Paragraph />);
-    const paragraph = wrapper.find('p');
-    //expect(wrapper.find('<Nav.Link>')).tohavelength(5);
-    expect(paragraph.text()).toEqual('Test React');
-  });
-});
+// describe('<MainNav />', () => {
+//   it('should render 5 navigation items', () => {
+//     const wrapper = shallow(<MainNav />);
+//     expect(wrapper.find('<Nav.Link>')).toHaveLength(5);
+//   });
+// });
