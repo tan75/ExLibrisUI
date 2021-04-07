b28d3ee8bcaf9e34fdffbc3f6aa5921a1a296aa1
Tetiana <ttobin@protonmail.ch>
2021-04-07 19:40:50 +0100

    Delete Bootstrap

--- exlibrisui/src/App.js
+++ exlibrisui/src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import MainNav from './components/MainNav/MainNav';
-import MainContent from './components/MainContent/MainContent';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <MainNav />
-      <MainContent />
-    </BrowserRouter>
+    <div>
+      <p>Test1</p>
+    </div>
   );
 };
 
--- exlibrisui/src/utils/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: process.env.REACT_APP_BASE_API_URL || 'http://localhost:8000/',
-});
-
-export default api;
